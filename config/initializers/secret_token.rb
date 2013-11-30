3e1e94d8aa82f693c31321a20b5f66baa2ca4fa8
just3obad <just3obad@gmail.com>
2013-12-01 01:47:41 +0200

    Empty App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BdayaAdmin::Application.config.secret_token = 'e70b469fabc61a97786501948b02cc4dc90724a5c6734ef092f789ce5a762ac3f3cb7c172e8dd8230f878c47be191eb41757e2526d76dfc72717584eb523d5eb'
