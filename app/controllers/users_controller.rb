02ab91bc22a536eef8feb8509b5184031ce26ba0
Menisy <elmenisy@gmail.com>
2013-12-16 03:51:30 +0200

    scopes back to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,10 @@ class UsersController < ApplicationController
     client = GOOGLE_CLIENT
     @auth_url = client.auth_code.authorize_url(
     redirect_uri: GOOGLE_REDIRECT_URI,
-    scope: GOOGLE_SCOPES)
+    scope:
+        "https://docs.google.com/feeds/ " +
+        "https://docs.googleusercontent.com/ " +
+        "https://spreadsheets.google.com/feeds/")
   end
 
 
