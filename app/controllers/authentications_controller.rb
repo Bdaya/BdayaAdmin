a7a389b259aa4e1476e6773489296d8b2d55b97f
Menisy <elmenisy@gmail.com>
2013-12-16 04:16:37 +0200

    fixed typo

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,7 +1,7 @@
 class AuthenticationsController < ApplicationController
   
   def create
-    client = GOOGLE
+    client = GOOGLE_CLIENT
     auth_token = client.auth_code.get_token(
     params[:code], redirect_uri: GOOGLE_REDIRECT_URI)
     session = GoogleDrive.login_with_oauth(auth_token.token)
