14b8ee7adf55ab03fce53022b369a0ac69c9ddb0
Menisy <elmenisy@gmail.com>
2013-12-16 03:33:18 +0200

    changed email cred

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -2,8 +2,8 @@ class AuthenticationsController < ApplicationController
   
   def create
     client = GOOGLE_CLIENT
-    #auth_token = client.auth_code.get_token(
-    #params[:code], redirect_uri: GOOGLE_REDIRECT_URI)
+    auth_token = client.auth_code.get_token(
+    params[:code], redirect_uri: GOOGLE_REDIRECT_URI)
     session = GoogleDrive.login_with_oauth(params[:code])
     @files = session.files
   end
