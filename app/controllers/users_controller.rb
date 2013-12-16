7f612a9357137a5439227cf83dda02dcad849c65
Menisy <elmenisy@gmail.com>
2013-12-16 02:52:05 +0200

    Moved google scopes to cred file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,12 @@ class UsersController < ApplicationController
   def home
     client = OAuth2::Client.new(
     GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET,
-    :site => "https://accounts.google.com",
-    :token_url => "/o/oauth2/token",
-    :authorize_url => "/o/oauth2/auth")
+    site: "https://accounts.google.com",
+    token_url: "/o/oauth2/token",
+    authorize_url: "/o/oauth2/auth")
     @auth_url = client.auth_code.authorize_url(
-    :redirect_uri => GOOGLE_REDIRECT_URI,
-    :scope =>
-        "https://docs.google.com/feeds/ " +
-        "https://docs.googleusercontent.com/ " +
-        "https://spreadsheets.google.com/feeds/")
+    redirect_uri: GOOGLE_REDIRECT_URI,
+    scope: GOOGLE_SCOPES)
   end
 
 
