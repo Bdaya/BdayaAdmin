3742eb53f6dc3f8c1fa0c65ccae49dfef68cebe5
Menisy <elmenisy@gmail.com>
2013-12-16 03:56:06 +0200

    added private scopeg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     @auth_url = client.auth_code.authorize_url(
     redirect_uri: GOOGLE_REDIRECT_URI,
     scope:
+        "https://docs.google.com/feeds/default/private/ " +
         "https://docs.google.com/feeds/ " +
         "https://docs.googleusercontent.com/ " +
         "https://spreadsheets.google.com/feeds/")
