7e3202177500511645dbd3f9b4eb39d09835e69b
Menisy <elmenisy@gmail.com>
2013-12-16 03:09:18 +0200

    added client to authentications controller

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,7 +1,11 @@
 class AuthenticationsController < ApplicationController
   
   def create
-    require 'google_drive'
+    client = OAuth2::Client.new(
+    GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET,
+    site: "https://accounts.google.com",
+    token_url: "/o/oauth2/token",
+    authorize_url: "/o/oauth2/auth")
     auth_token = client.auth_code.get_token(
     params[:code], redirect_uri: GOOGLE_REDIRECT_URI)
     session = GoogleDrive.login_with_oauth(auth_token.token)
