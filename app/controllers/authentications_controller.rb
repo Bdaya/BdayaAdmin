b93110ba49b44408b1cfdd4d4b03cb1942fc7e68
Menisy <elmenisy@gmail.com>
2013-12-16 03:27:34 +0200

    Trying to fix authentication error

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -2,9 +2,9 @@ class AuthenticationsController < ApplicationController
   
   def create
     client = GOOGLE_CLIENT
-    auth_token = client.auth_code.get_token(
-    params[:code], redirect_uri: GOOGLE_REDIRECT_URI)
-    session = GoogleDrive.login_with_oauth(auth_token.token)
+    #auth_token = client.auth_code.get_token(
+    #params[:code], redirect_uri: GOOGLE_REDIRECT_URI)
+    session = GoogleDrive.login_with_oauth(params[:code])
     @files = session.files
   end
 end
