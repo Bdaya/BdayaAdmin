7aac188786cd84a925b3b368c0a644f2adebd49d
Menisy <elmenisy@gmail.com>
2013-12-16 03:06:03 +0200

    Added require google_drive in authentications controller

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,6 +1,7 @@
 class AuthenticationsController < ApplicationController
-
+  
   def create
+    require 'google_drive'
     auth_token = client.auth_code.get_token(
     params[:code], redirect_uri: GOOGLE_REDIRECT_URI)
     session = GoogleDrive.login_with_oauth(auth_token.token)
