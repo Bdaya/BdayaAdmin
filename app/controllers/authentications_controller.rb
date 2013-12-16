ffa8046509c57630c5a55558d0228d32108d2493
Menisy <elmenisy@gmail.com>
2013-12-16 04:11:39 +0200

    back to normal

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,10 +1,10 @@
 class AuthenticationsController < ApplicationController
   
   def create
-    client = GOOGLE_CLIENT
+    client = GOOGLE
     auth_token = client.auth_code.get_token(
     params[:code], redirect_uri: GOOGLE_REDIRECT_URI)
-    session = GoogleDrive.login_with_oauth(params[:code])
+    session = GoogleDrive.login_with_oauth(auth_token.token)
     @files = session.files
   end
 end
