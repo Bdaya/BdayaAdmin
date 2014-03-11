2cdc73d51b7e7c4726094082f222a7ea439bc7d5
AhmedZaleh <ahmedzaleh7@gmail.com>
2014-03-11 21:18:20 +0200

    modified kids courses assessments

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -90,7 +90,6 @@ class KidsController < ApplicationController
         CourseGrade.create(:course => course, :kid => @kid, :grade => params["#{course.name}"])
       end
     end
-
     redirect_to kids_url
   end
 
@@ -107,7 +106,7 @@ class KidsController < ApplicationController
         CourseGrade.create(:course => course, :kid => @kid)
       end
     end
-    
+
     #time = ((Time.now - @courses.last.course_grades.last.created_at)/(60*60*24)).to_i
     #if (time > 7)
       #@week =+ 1
