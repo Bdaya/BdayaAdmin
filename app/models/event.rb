e0f3bf76cb35ea2eee968edf42a80bdfac5110e4
Diab <diab977@gmail.com>
2014-03-24 15:11:24 +0200

    adding events requests and forms

--- app/models/event.rb
+++ app/models/event.rb
@@ -21,8 +21,8 @@ class Event
 
   has_and_belongs_to_many :cases
   belongs_to :committee
-  has_one :creator, class_name: 'User', inverse_of: :created_events
-  has_one :project_manager, class_name: 'User', inverse_of: :managed_events
+  belongs_to :creator, class_name: 'User', inverse_of: :created_events
+  belongs_to :project_manager, class_name: 'User', inverse_of: :managed_events
   has_and_belongs_to_many :members, class_name: 'User', inverse_of: :member_of_events
   has_many :requests
   has_many :event_images
@@ -35,4 +35,13 @@ class Event
   def cover_pic
     img = event_images.where(cover: true).first
   end
+
+  def get_materials_requests
+    self.requests.where(request_type: 'materials').desc(:time).to_a
+  end
+
+  def get_permissions_requests
+    self.requests.where(request_type: 'permissions').desc(:time).to_a
+  end
+
 end
