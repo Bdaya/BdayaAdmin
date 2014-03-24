de54372d4e3bd80013b0fddd09d600007198d135
just3obad <just3obad@gmail.com>
2014-03-24 15:47:25 +0200

    Meetings Validation

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,8 +1,13 @@
 class MeetingsController < ApplicationController
 
-  # authorize_actions_for Meeting
-  
+  # authorize_actions_for Meeting  
   def index
+    if Rails.cache.read("ERROR").nil?
+      @ERROR = []
+      Rails.cache.write("ERROR","[]")
+    else
+      @ERROR = Rails.cache.read("ERROR")
+    end
     @meetings = Meeting.desc(:date).all.to_a
     @today_meetings = current_user.get_today_meetings
     @tomorrow_meetings = current_user.get_tomorrow_meetings
@@ -50,14 +55,19 @@ class MeetingsController < ApplicationController
     @request.roomnumber = @meeting.location
 
     if (@meeting.save && @request.save)
-      @meeting.attendees.each do |a|
-        Attendance.create_attendance(@meeting,a)   
-      end
+        @meeting.attendees.each do |a|
+          Attendance.create_attendance(@meeting,a)   
+    end
       redirect_to action: 'index'
     else
+      @ERROR = []
+      @meeting.errors.full_messages.each do |msg|
+        @ERROR << msg
+      end      
+      Rails.cache.write("ERROR", @ERROR)
       @meeting.destroy
       @request.destroy
-      render  'new' 
+      redirect_to action: 'index'
     end
   end
 
