ade142a510ac8b074bd7e5ed32b0ec055070aadc
just3obad <just3obad@gmail.com>
2014-03-24 13:37:27 +0200

    Discussion

--- app/authorizers/meeting_authorizer.rb
+++ app/authorizers/meeting_authorizer.rb
@@ -1,11 +1,11 @@
 class MeetingAuthorizer < ApplicationAuthorizer
 
   def self.creatable_by?(user)
-    user.head?
+    # user.head?
+    true
   end
 
   def self.readable_by?(user)
     true
   end
-
 end
--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,6 +1,6 @@
 class MeetingsController < ApplicationController
 
-  authorize_actions_for Meeting
+  # authorize_actions_for Meeting
   
   def index
     @meetings = Meeting.desc(:date).all.to_a
