ad475861151bad621701414ea19b89c0877ff630
Menisy <elmenisy@gmail.com>
2014-04-15 17:59:51 +0200

    css fix

---
+++ app/authorizers/task_authorizer.rb
@@ -0,0 +1,11 @@
+class TaskAuthorizer < ApplicationAuthorizer
+
+  def self.creatable_by?(user)
+    user.head? || user.vice_head? ||
+    user.upper_board? || user.president?
+  end
+
+  def self.readable_by?(user)
+    true
+  end
+end
